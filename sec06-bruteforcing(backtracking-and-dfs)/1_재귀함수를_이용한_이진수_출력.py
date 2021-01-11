e2201adbe4ecc67f0467f7cf1310ec871b3af306
gumsu <chavi9@naver.com>
2021-01-11 20:55:29 +0900

    sec06-01. 재귀함수를 이용한 이진수 출력

---
+++ sec06-bruteforcing(backtracking-and-dfs)/1_재귀함수를_이용한_이진수_출력.py
@@ -0,0 +1,16 @@
+import sys
+
+for i in range(1,6):
+    sys.stdin = open(f'in{i}.txt',"rt")
+
+    def DFS(x):
+        if x == 0:
+            return
+        else:
+            DFS(x//2)
+            print(x%2, end='')
+    
+    if __name__ == "__main__":
+        n = int(input())
+        DFS(n)
+        print()

dcec00b7612ee806b724bd67fb7d825334bee836
gumsu <chavi9@naver.com>
2021-02-18 19:19:16 +0900

    sec08-13. 회장뽑기(플로이드 워샬 응용)

---
+++ sec08-dynamic-programming/13_회장뽑기(플로이드_워샬_응용).py
@@ -0,0 +1,35 @@
+n = int(input())
+dis = [[100]*(n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    dis[i][i] = 0
+
+while True:
+    a, b = map(int, input().split())
+    if a == -1 and b == -1 :
+        break
+    dis[a][b] = 1
+    dis[b][a] = 1
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j])
+
+# for x in dis:
+#     print(x)
+
+res = [0]*(n+1)
+score = 100
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        res[i] = max(res[i], dis[i][j])
+    score = min(score, res[i])
+
+out = []
+for i in range(1, n+1):
+    if res[i] == score:
+        out.append(i)
+print(score, len(out))
+print(*out)

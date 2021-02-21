2564644b683ffada415242528ef9022139361917
gumsu <chavi9@naver.com>
2021-02-21 20:58:47 +0900

    [#1] console 창에 격자판 출력

--- sec09-simulation/block_game.py
+++ sec09-simulation/block_game.py
@@ -5,5 +5,8 @@ if __name__ == "__main__":
     sc = t.Screen()
     sc.bgcolor("black")
     sc.setup(width=600, height=700)
+    grid = [[0]*12 for _ in range(24)]
+    for x in grid:
+        print(x)
 
     sc.mainloop()

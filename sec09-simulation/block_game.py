95904853c5da3332528c993518b68c94187a5462
gumsu <chavi9@naver.com>
2021-02-21 21:00:49 +0900

    [#1] 숫자 7로 아래,왼,오른 벽 생성

--- sec09-simulation/block_game.py
+++ sec09-simulation/block_game.py
@@ -6,6 +6,11 @@ if __name__ == "__main__":
     sc.bgcolor("black")
     sc.setup(width=600, height=700)
     grid = [[0]*12 for _ in range(24)]
+    for i in range(24):
+        grid[i].insert(0,7)
+        grid[i].append(7)
+    grid.append([7]*14)
+
     for x in grid:
         print(x)
 

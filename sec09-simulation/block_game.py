5b0bfe6b49bd00c4204c2c11b03cfd0feb11b41e
gumsu <chavi9@naver.com>
2021-02-21 21:16:32 +0900

    [#1] 스크린에 전체 블록 출력

--- sec09-simulation/block_game.py
+++ sec09-simulation/block_game.py
@@ -1,6 +1,16 @@
 import turtle as t
 import random as r
 
+def draw_grid(black, grid):
+    top = 250
+    left = -150
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            sc_x = left+(x*22)
+            sc_y = top-(y*22)
+            block.goto(sc_x,sc_y)
+            block.stamp()
+
 if __name__ == "__main__":
     sc = t.Screen()
     sc.bgcolor("black")
@@ -16,6 +26,8 @@ if __name__ == "__main__":
     block.speed(0)
     block.shape("square")
     block.color("red")
+
+    draw_grid(block, grid)
 
     for x in grid:
         print(x)
